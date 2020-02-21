59f0766482512ff5d059cf6270d99d5d2116dd54
yasnakateb <yasnakatebzadeh@gmail.com>
2020-02-21 22:10:58 +0330

    read_data.v: Add states

--- read_data.v
+++ read_data.v
@@ -4,36 +4,46 @@ module read_data#(
   	parameter 
   			INPUT_FILE  = "input_picture.hex", 	
   			IMAGE_WIDTH = 768, 					
-			IMAGE_HEIGHT = 512, 						
-			START_DELAY = 100, 				
-			HORIZONTAL_DELAY = 160,					
+			IMAGE_HEIGHT = 512,
+			// Delay during start up time 						
+			STATEART_DELAY = 100,
+			// Delay between Horizontal synchronous pulses	 				
+			HORIZONTAL_SYNC_DELAY = 160,
+			// Threshold value for Threshold operation					
 			THRESHOLD= 90						
 	)
 	(
 		clk,												
 		reset,									
-		data_R0,				
-	    data_G0,				
-	    data_B0,				
-	    data_R1,				
-	    data_G1,				
-	    data_B1,
+		data_R_Even,				
+	    data_G_Even,				
+	    data_B_Even,				
+	    data_R_Odd,				
+	    data_G_Odd,				
+	    data_B_Odd,
 	    vertical_Pulse,							
 		horizontal_Pulse,							
 		done_Flag					
 	);
 
-	input clk;										
-	input reset;									
-	output reg [7:0]  data_R0;				
-    output reg [7:0]  data_G0;			
-    output reg [7:0]  data_B0;				
-    output reg [7:0]  data_R1;				
-    output reg [7:0]  data_G1;				
-    output reg [7:0]  data_B1;
-    output vertical_Pulse;								
+	input clk;
+	// Active low										
+	input reset;
+	// 8 bit Red, Green, Blue  data (Even)									
+	output reg [7:0]  data_R_Even;				
+    output reg [7:0]  data_G_Even;			
+    output reg [7:0]  data_B_Even;
+    // 8 bit Red, Green, Blue  data (Odd)						
+    output reg [7:0]  data_R_Odd;				
+    output reg [7:0]  data_G_Odd;				
+    output reg [7:0]  data_B_Odd;
+    // Vertical synchronous pulse
+    output vertical_Pulse;	
+    // Horizontal synchronous pulse							
 	output reg horizontal_Pulse;						
 	output done_Flag;
 
-
+	reg [1:0] current_STATE;						
+	reg [1:0] next_STATE;
+	
 endmodule					
