78d34c3514a1bbfa12ef0ae431ec0a4fbdf4a69e
yasnakateb <yasnakatebzadeh@gmail.com>
2020-02-25 11:25:11 +0330

    Add module write_data

---
+++ hdl/write_data.v
@@ -0,0 +1,11 @@
+module write_data
+    #(parameter 
+        OUTPUT_FILE  = "output_picture.bmp",                        
+        IMAGE_WIDTH = 768,                          
+        IMAGE_HEIGHT = 512                          
+    )
+    (
+    
+    );  
+
+endmodule
