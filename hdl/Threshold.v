fb0081259676cb99742bc962a722ce58d881aaf7
yasnakateb <yasnakatebzadeh@gmail.com>
2020-02-25 11:25:22 +0330

    Add module Threshold

---
+++ hdl/Threshold.v
@@ -0,0 +1,23 @@
+// **************************************************
+// ***************** Todo *************************** 
+// **************************************************
+
+// Delete parameters from read_data and write_data 
+
+module Threshold
+#(
+    parameter 
+        INPUT_FILE  = "input_picture.hex",  
+        IMAGE_WIDTH = 768,                  
+        IMAGE_HEIGHT = 512
+    )
+    (
+        clk,                                                
+        reset,                                  
+                    
+    );
+
+    input clk;
+    input reset;
+
+endmodule 
