eb7dc668c7c8576d721923ec90887280c1480902
yasnakateb <yasnakatebzadeh@gmail.com>
2020-02-25 21:20:32 +0330

    write_data.v: Add inputs/outputs

--- hdl/write_data.v
+++ hdl/write_data.v
@@ -5,7 +5,38 @@ module write_data
         IMAGE_HEIGHT = 512                          
     )
     (
-    
-    );  
+        clk,                                          
+        reset,                                          
+        horizontal_Pulse,                                                  
+        data_Red_Even,                
+        data_Green_Even,                
+        data_Blue_Even,                
+        data_Red_Odd,             
+        data_Green_Odd,             
+        data_Blue_Odd,
+        sig_Write_Done
+    ); 
+
+    input clk; 
+    // Active low                                            
+    input reset; 
+    // Horizontal synchronous pulse                                          
+    input horizontal_Pulse;
+       
+    /*
+     * We Use two pixels at the same time because each data has three different components 
+     * which is red, green and blue.
+     */  
+
+    // 8 bit Red, Green, Blue  data (Even)                                                                         
+    input  data_Red_Even;              
+    input [7:0]  data_Green_Even;          
+    input [7:0]  data_Blue_Even;
+    // 8 bit Red, Green, Blue  data (Odd)                       
+    input [7:0]  data_Red_Odd;               
+    input [7:0]  data_Green_Odd;               
+    input [7:0]  data_Blue_Odd;
+
+    output reg sig_Write_Done;
 
 endmodule
