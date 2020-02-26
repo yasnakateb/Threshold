d1fe0085d634bc3ca3df75cc7d80c10a357753ee
yasnakateb <yasnakatebzadeh@gmail.com>
2020-02-27 00:04:32 +0330

    Add TestBench_1.v

---
+++ hdl/TestBenches/TestBench_1.v
@@ -0,0 +1,27 @@
+`timescale 1ns / 1ps
+
+module TestBench_1;
+
+	// Inputs
+	reg clk;
+	reg reset;
+
+	// Instantiate the Unit Under Test (UUT)
+	Image_Processing uut (
+		.clk(clk), 
+		.reset(reset)
+	);
+	initial begin 
+		clk = 0;
+		forever #10 clk = ~clk;
+	end
+
+	initial begin
+		reset     = 0;
+		#25 reset = 1;
+	end
+
+	
+      
+endmodule
+
